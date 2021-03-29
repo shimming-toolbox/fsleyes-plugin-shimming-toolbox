c1fb02e1902a06325cbb7d2f81aa95445512f3a5
Ainsleigh Hill <ainsleighhill@gmail.com>
2021-03-29 12:35:25 -0700

    add info message for pipx install

--- installer/install_pipx.sh
+++ installer/install_pipx.sh
@@ -8,3 +8,5 @@ VENV=pst_venv_$VENV_ID
 yes | conda activate $VENV
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
+
+echo "Package pipx successfully installed. To use pipx, restart your shell."
