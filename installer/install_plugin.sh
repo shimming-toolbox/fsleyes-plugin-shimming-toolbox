a2fb63c73d8d67bc9a7c2f32012a3eca932f5523
Ainsleigh Hill <ainsleighhill@gmail.com>
2021-08-03 12:00:53 -0700

    add coloured info/error messages to install.sh

--- installer/install_plugin.sh
+++ installer/install_plugin.sh
@@ -8,6 +8,49 @@ ST_DIR=$HOME/shimming_toolbox
 PYTHON_DIR=python
 BIN_DIR=bin
 
+function print() {
+  type=$1; shift
+  case "$type" in
+  # Display useful info (green)
+  info)
+    echo -e "\n\033[0;32m${*}\033[0m\n"
+    ;;
+  # To interact with user (no carriage return) (light green)
+  question)
+    echo -e -n "\n\033[0;92m${*}\033[0m"
+    ;;
+  # To display code that is being run in the Terminal (blue)
+  code)
+    echo -e "\n\033[0;34m${*}\033[0m\n"
+    ;;
+  # Warning message (yellow)
+  warning)
+    echo -e "\n\033[0;93m${*}\033[0m\n"
+    ;;
+  # Error message (red)
+  error)
+    echo -e "\n\033[0;31m${*}\033[0m\n"
+    ;;
+  esac
+}
+
+# Elegant exit with colored message
+function die() {
+  print error "$1"
+  exit 1
+}
+
+# Run a command and display it in color. Exit if error.
+# @input: string: command to run
+function run() {
+  ( # this subshell means the 'die' only kills this function and not the whole script;
+    # the caller can decide what to do instead (but with set -e that usually means terminating the whole script)
+    print code "$@"
+    if ! "$@" ; then
+      die "ERROR: Command failed."
+    fi
+  )
+}
 
 # Gets the shell rc file path based on the default shell.
 # @output: THE_RC and RC_FILE_PATH vars are modified
@@ -60,17 +103,19 @@ conda activate $VENV
 # set -u
 
 # Install fsleyes
+print info "Installing fsleyes"
 yes | conda install -c conda-forge fsleyes=0.34.2
 
 # Downgrade wxpython version due to bugs
+print info "Downgrading wxpython to 4.0.7"
 yes | conda install -c conda-forge wxpython=4.0.7
 
 # Install fsleyes-plugin-shimming-toolbox
-echo "Installing fsleyes-plugin-shimming-toolbox"
-python -m pip install .
+print info "Installing fsleyes-plugin-shimming-toolbox"
+run python -m pip install .
 
 # Create launchers
-echo "Creating launcher for fsleyes-plugin-shimming-toolbox..."
+print info "Creating launcher for fsleyes-plugin-shimming-toolbox"
 mkdir -p $ST_DIR/$BIN_DIR
 # echo $ST_DIR/python/envs/$VENV/bin/*st_*
 chmod +x shimming-toolbox.sh
@@ -81,4 +126,4 @@ export PATH=$ST_DIR/$BIN_DIR:$PATH
 
 edit_shellrc
 
-echo "Open a new Terminal window to load environment variables, or run: source $RC_FILE_PATH"
+print info "Open a new Terminal window to load environment variables, or run: source $RC_FILE_PATH"
