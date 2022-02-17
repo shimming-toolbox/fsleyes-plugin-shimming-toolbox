4d3cdde899869dfbc94eefa6366c3f73632ac9b7
Alexandre D'Astous <po09i@hotmail.com>
2022-02-17 13:45:53 -0500

    Update version of ST being downloaded

--- installer/install_shimming_toolbox.sh
+++ installer/install_shimming_toolbox.sh
@@ -11,11 +11,15 @@ cd $ST_DIR
 
 print info "Downloading Shimming-Toolbox"
 
-curl -L https://github.com/shimming-toolbox/shimming-toolbox/archive/refs/heads/master.zip > shimming-toolbox-master.tar.gz
-#gunzip -c shimming-toolbox-master.tar.gz | tar xopf -
-#unzip for now
-unzip -o shimming-toolbox-master.tar.gz
-cd shimming-toolbox-master
+ST_VERSION=f9e272841af5b2798214170b314c342e1174fb79
+
+curl -L "https://github.com/shimming-toolbox/shimming-toolbox/archive/${ST_VERSION}.zip" > "shimming-toolbox-${ST_VERSION}.zip"
+
+# Froze a commit, we can select a release later
+# gunzip -c "shimming-toolbox-${ST_VERSION}.tar.gz" | tar xopf -
+# unzip for now, when we use releases we can use the tar and gunzip
+unzip -o "shimming-toolbox-${ST_VERSION}.zip"
+cd "shimming-toolbox-${ST_VERSION}"
 make install
 
 print info "To launch the plugin, load the environment variables then run: shimming-toolbox"
