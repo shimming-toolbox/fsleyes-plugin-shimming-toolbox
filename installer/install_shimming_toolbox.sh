7f634d3a46d3c7915839d7a3732524ccedd5760f
Nick <nick@kousu.ca>
2022-04-06 15:56:53 -0400

    Bump shimming-toolbox to use a single-env
    See https://github.com/shimming-toolbox/shimming-toolbox/pull/379/

--- installer/install_shimming_toolbox.sh
+++ installer/install_shimming_toolbox.sh
@@ -14,7 +14,7 @@ rm -rf "${ST_DIR}/shimming-toolbox"
 
 print info "Downloading Shimming-Toolbox"
 
-ST_VERSION=0b105fc3e0f9ac366ef52feabb30adc4be9a463e
+ST_VERSION=e93080e2ea29fcfd430066cb6c92d75689eec5b0
 
 curl -L "https://github.com/shimming-toolbox/shimming-toolbox/archive/${ST_VERSION}.zip" > "shimming-toolbox-${ST_VERSION}.zip"
 
