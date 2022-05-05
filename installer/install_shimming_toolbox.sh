5a09c5d4d16a7bc805782e0cdb80becc7650bd22
kousu <nick@kousu.ca>
2022-05-05 10:19:32 -0400

    Version bump

--- installer/install_shimming_toolbox.sh
+++ installer/install_shimming_toolbox.sh
@@ -14,7 +14,7 @@ rm -rf "${ST_DIR}/shimming-toolbox"
 
 print info "Downloading Shimming-Toolbox"
 
-ST_VERSION=f53d61ab8ded893090809a927e260c1390c3d640
+ST_VERSION=f7e00f9679d43e7dacbc52242d37f9d5e426e7d9
 
 curl -L "https://github.com/shimming-toolbox/shimming-toolbox/archive/${ST_VERSION}.zip" > "shimming-toolbox-${ST_VERSION}.zip"
 
