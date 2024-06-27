7b42616140a377fc62e4a3cba1d46a2a10edb403
Arnaud Breheret <arnaudbreheret14@gmail.com>
2024-06-27 14:10:05 -0400

    Change to updated commmit on ST

--- installer/install_shimming_toolbox.sh
+++ installer/install_shimming_toolbox.sh
@@ -14,7 +14,7 @@ rm -rf "${ST_DIR}/shimming-toolbox"
 
 print info "Downloading Shimming-Toolbox"
 
-ST_VERSION="420905677bf624228e2e1088d842178de1f30196"
+ST_VERSION="cb579afefb8ac742909e5d706a85c2c6dc091274"
 
 curl -L "https://github.com/shimming-toolbox/shimming-toolbox/archive/${ST_VERSION}.zip" > "shimming-toolbox-${ST_VERSION}.zip"
 
